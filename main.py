2e18f548480bf7b06b716082438800e82cbbbe9b
ModernMon <lakshmiaravind.atom@gmail.com>
2023-05-23 11:39:22 +0530

    used pandas for working with csv files

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ st.set_page_config(layout="wide")
 col1, col2 = st.columns(2)
 
 with col1:
-    st.image("images/aravind.jpg")
+    st.image("images/aravind.jpg", width=500)
 
 with col2:
 
@@ -31,16 +31,22 @@ content2 ="""Below you can find some of the apps I built in python.
 Feel free to contact me anytime..! Thats how I work with python.
 
 """
-st.write(content2)
+st.info(content2)
 
-col3,col4 = st.columns(2)
+col3,empty_col,col4 = st.columns([1.5,0.5,1.5])
 
 df = pandas.read_csv("data.csv", sep=";")
 
 with col3:
     for index, row in df[:10].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[source code]({row['url']})")
 
 with col4:
     for index, row in df[10:].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[source code]({row['url']})")
